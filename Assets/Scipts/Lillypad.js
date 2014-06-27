9d55d50549622369fe709c04bbb686bea0b6e515
cearp <chelsea-e@live.com>
2014-06-26 22:37:17 -0700

    Pushing relevant files
    Only upload ProjectSettings and Assets folders, and nothing else. The
    gitignore file should do this.

---
+++ Assets/Scipts/Lillypad.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public var LP_hover : Texture2D;
+public var LP_selected : Texture2D;
+public var LP_static : Texture2D;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ Assets/Scipts/Load.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+//load in start menu buttons
+public var start : Texture2D;
+public var load : Texture2D;
+
+//create a dynamic resolution so that everything changes with screen size.
+private var wratio = 0.0;
+private var hratio = 0.0;
+
+function Start () {
+//set resolution
+	wratio = Screen.width; 
+	hratio = Screen.height;
+	wratio = wratio / 750;
+	hratio = hratio / 400;
+
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+if (GUI.Button (Rect (40*wratio,320*hratio,128,58),start)){
+print("start");
+//The 1 value is set in the file>build settings for the main game scene
+Application.LoadLevel (1);
+}
+if (GUI.Button (Rect (540*wratio,320*hratio,128,58),load))
+print("load");
+}
